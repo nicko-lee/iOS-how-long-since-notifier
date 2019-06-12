01ea2ce420c8cc0035687ec645b5783764dde5ea
Nicko Lee <nicko.lee@anz.com>
2019-06-13 09:59:05 +1000

    Initial Commit

---
+++ How Long Since Notifier/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  How Long Since Notifier
+//
+//  Created by Nicko Lee on 6/13/19.
+//  Copyright © 2019 Nicko Lee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
